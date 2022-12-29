e866c7d4874deeccd1c2416e7e872eb825168863
Jakub Strzelecki <89380172+Arutorek@users.noreply.github.com>
2022-12-29 23:00:55 +0000

    add keep alive for online hosting and litle changes

--- bot.py
+++ bot.py
@@ -2,21 +2,29 @@ import discord
 import main_bot_logic
 import json
 from discord.ext import commands
+from keep_alive import keep_alive
 
 
 def run_discord_bot():
-    with open('config.json', 'r') as cfg:
-        TOKEN = json.load(cfg)
-    bot = commands.Bot(command_prefix='$', intents=discord.Intents.all(), case_insensitive=True, self_bot=True)
+  with open('config.json', 'r') as cfg:
+    TOKEN = json.load(cfg)
+  bot = commands.Bot(command_prefix='$',
+                     intents=discord.Intents.all(),
+                     case_insensitive=True,
+                     self_bot=True)
 
-    @bot.command(name="start")
-    async def geting_wot_news_from_rykoszet_webpage(ctx):
-        base_url = None
-        while True:
-            url = main_bot_logic.main(base_url)
-            base_url = url
-            channel = bot.get_channel(1053402661967368203)
-            await channel.send(url)
+  @bot.command(name="start")
+  async def geting_wot_news_from_rykoszet_webpage(ctx):
+    base_url = None
+    while True:
+      is_update, url = main_bot_logic.main(base_url)
+      if is_update:
+        base_url = url
+        channel = bot.get_channel(1053402661967368203)
+        await channel.send(url)
+      else:
+        channel = bot.get_channel(1054842860144431134)
+        await channel.send("No update form 'rykoszet.info' yet")
 
-
-    bot.run(TOKEN["token"])
+  keep_alive()
+  bot.run(TOKEN["token"])
--- main_bot_logic.py
+++ main_bot_logic.py
@@ -2,11 +2,11 @@ import requests
 from bs4 import BeautifulSoup
 import time
 
-
 base_url = 'https://rykoszet.info'
 
 headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36'
+  'User-Agent':
+  'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36'
 }
 
 page = requests.get(base_url, headers=headers)
@@ -17,33 +17,30 @@ data_from_page = []
 
 
 def web_scraper_wot():
-    data_elements = soup.find('div', class_='post-inner post-hover')
-    return data_elements
+  data_elements = soup.find('div', class_='post-inner post-hover')
+  return data_elements
 
 
 def get_link_of_data_to_post(data_elements):
-    a = data_elements.find('a', href=True)
-    url = a['href']
-    return url
+  a = data_elements.find('a', href=True)
+  url = a['href']
+  return url
 
 
 def checking_update(url, last_url):
-    if url == last_url or url == None:
-        return False
-    else:
-        return True
+  if url == last_url or url == None:
+    return False
+  else:
+    return True
 
 
 def main(last_url):
-    while True:
-        time.sleep(300)
-        data_elements = web_scraper_wot()
-        url = get_link_of_data_to_post(data_elements)
-        is_update = checking_update(url, last_url)
-        print("new update status is: " + is_update)
-        if is_update:
-            last_url = url
-            return url
-        
+  time.sleep(10)
+  data_elements = web_scraper_wot()
+  url = get_link_of_data_to_post(data_elements)
+  is_update = checking_update(url, last_url)
+  return is_update, url
+
+
 if __name__ == "__main__":
-    main(last_url=None)
+  main(last_url=None)
