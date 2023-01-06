16306f3cd3190f2c5cddc0b7bec3d2c9607ad358
Arutorek <arutorek@gmail.com>
2023-01-06 15:39:46 +0100

    add command help

--- bot.py
+++ bot.py
@@ -11,6 +11,7 @@ bot = commands.Bot(command_prefix='$',
                     intents=discord.Intents.all(),
                     case_insensitive=True,
                     self_bot=True)
+bot.remove_command("help")
 
 
 @bot.event
@@ -29,6 +30,13 @@ async def geting_wot_news_from_rykoszet_webpage():
       for url in urls:
         channel = bot.get_channel(1053402661967368203)
         await channel.send(url)
+
+
+@bot.command(name="help")
+async def help(ctx):
+    embed = discord.Embed(title="Help", description="Pomoc od BotDoWot")
+    embed.add_field(name="Główne zadania bota:", value="Pobieranie  z internetu i wysyłanie na serwer informacji o nowościach w grze World of Tanks.")
+    await ctx.channel.send(embed=embed)
 
 keep_alive()
 bot.run(TOKEN["token"])
